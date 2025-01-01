61a7be3cfd76346580b78daea7a59a4b935d4b04
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-01-02 02:46:13 +0600

    Compleated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8000;
 
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["https://chat-app-frontend-psi-three.vercel.app"],
         credentials: true,
     })
 );
